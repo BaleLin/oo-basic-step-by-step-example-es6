802829dd8abbbc08178fe72f95c71d6d4518cfbf
BaleLin <bale.lin@oocl.com>
2018-07-09 22:12:06 +0800

    11 diffuclt

--- src/practice_11/class.js
+++ src/practice_11/class.js
@@ -1,1 +1,53 @@
 
+
+import Person from "../practice_11/person";
+import Student from "../practice_11/student";
+
+export default  class klass{
+    leader;
+    constructor(number){
+        this.number = number;
+    }
+
+    getDisplayName(){
+        return "Class " + this.number
+    }
+
+    assignLeader(student){
+        if(this.number === student.klass.number){
+            this.leader = student;
+            if(student.klass.equal(this.assignLeaderListener.klass)){
+                console.log (`I am ${this.assignLeaderListener.name}. I know ${student.name} become Leader of Class ${this.number}.`)
+            }
+        }else{
+            this.leader = null;
+            console.log ("It is not one of us.")
+        } 
+    }
+
+    appendMember(student){
+        student.klass = this;
+        if(student.klass.equal(this.joinListener.klass)){
+            console.log (`I am ${this.joinListener.name}. I know ${student.name} has joined Class ${this.number}.`);
+        }
+    }
+
+    equal(peopleklass){
+        return this.number === peopleklass.number;
+    }
+
+    registerAssignLeaderListener(teacher){
+        this.assignLeaderListener = teacher;
+    }
+
+    registerJoinListener(teacher){
+        this.joinListener = teacher;
+    }
+}
+
+
+
+
+
+
+
--- src/practice_11/teacher.js
+++ src/practice_11/teacher.js
@@ -1,1 +1,31 @@
 
+import Person from "../practice_11/person";
+export default  class Teacher extends Person{
+    constructor(id,name, age,klasses){
+        super(id,name,age);
+        this.klasses = klasses;
+        // this.name = name;
+        // this.age = age;
+        
+    }
+     introduce() {
+        if(this.klasses == undefined || this.klasses.length === 0){
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }else{
+            let teachClass = this.klasses.map(klass => klass.number).join(", ");
+            return super.introduce() + " I am a Teacher. I teach Class "+ teachClass +".";
+} 
+    }
+
+    introduceWith(student){
+        let isStudent = this.klasses.some( klass => klass.equal(student.klass));
+        if(isStudent){
+            return super.introduce() +" I am a Teacher. I teach "+ student.name +".";
+        }else{
+            return super.introduce() + ` I am a Teacher. I don't teach ${student.name}.`;
+        }
+}
+}
+
+
+
